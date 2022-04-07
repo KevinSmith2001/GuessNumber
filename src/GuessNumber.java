50c1d8aa2474ddff16c3d1d3fca97d50370a7f7c
Kevin <kevinsmith2001233@gmail.com>
2022-04-07 17:36:19 +0800

    hello

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class GuessNumber {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to GuessNumber!");
+        System.out.println("Please enter an integer between 1-100");
+        int answer=(int)(Math.random()*100+1);
+        Scanner scan = new Scanner(System.in);
+        int guess = scan.nextInt();
+        if(guess>answer){
+            System.out.println("Big!");
+        }else if(guess<answer){
+            System.out.println("Small!");
+        }else{
+            System.out.println("Congratulations!You Guessed It Right!~~");
+        }
     }
 }
